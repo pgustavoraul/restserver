6d1ceac38877352dbd46dd93a65e2b9cb6469823
Gustavo <pgustavo_raul@yahoo.com>
2018-08-28 22:24:19 -0500

    funcion put

---
+++ server/routes/usuario.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+const app = express();
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuario')
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        //usuarioDB.password = null;
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    })
+
+})
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    })
+
+
+})
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuario')
+})
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 
 const bodyParser = require('body-parser')
@@ -8,34 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err
+    console.log('Base de datos ONLINE');
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el npmbre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
+});
 
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando el puerto ${process.env.PORT}`);
