21781cb1f3f51420710438aa2c4e35482260c920
Gustavo <pgustavo_raul@yahoo.com>
2018-09-03 23:13:34 -0500

    login jwt

--- server/server.js
+++ server/server.js
@@ -6,11 +6,13 @@ const app = express()
 
 const bodyParser = require('body-parser')
     // parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
 
-app.use(require('./routes/usuario'));
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err
