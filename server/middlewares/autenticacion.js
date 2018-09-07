4da9f2133760842f31de72d3c411a921f6f29645
Gustavo <pgustavo_raul@yahoo.com>
2018-09-06 22:39:25 -0500

    resuelto conflicto 1

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -6,7 +6,9 @@ const jwt = require('jsonwebtoken');
 
 let verificarToken = (req, res, next) => {
     let token = req.get('token');
+
     jwt.verify(token, process.env.SEED, (err, decoded) => {
+
         if (err) {
             return res.status(401).json({
                 ok: false,
@@ -18,27 +20,32 @@ let verificarToken = (req, res, next) => {
 
         req.usuario = decoded.usuario;
         next();
+
     })
-}
+
+};
 
 //=====================
 // Verifica AdminRole
 //=====================
 
 let verificaAdmin_Role = (req, res, next) => {
+
     let usuario = req.usuario;
 
     if (usuario.role === 'ADMIN_ROLE') {
         next();
     } else {
-        return res.status(401).json({
+        return res.json({
             ok: false,
             err: {
                 message: 'El role no es valido para esta función'
             }
         });
     }
-}
+};
+
+
 
 module.exports = {
     verificarToken,
